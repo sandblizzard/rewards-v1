caebd71fd02c62ad8ca0832c0a7082987194e8a5
0xksure <47750504+0xksure@users.noreply.github.com>
2023-01-11 13:20:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sandblizzard Monorepo
+
+This is the monorepo for the submission(s) to the [Sandstorm Solana Hackathon](https://www.sandstormhackathon.com/)
+
+The repo contains
+
+- The program for storing and managing payment of rewards
+- Client for indexing repositories and storing information on-chain - TBD
+- Remote transaction trigger - TBD
---
+++ tests/rewards-v1.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { RewardsV1 } from "../target/types/rewards_v1";
+
+describe("rewards-v1", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.RewardsV1 as Program<RewardsV1>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
